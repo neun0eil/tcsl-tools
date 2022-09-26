8da2a335c1a950494a88d88d31ba5d647e460308
neun0eil <neun0eil@nokoda.fr>
2022-09-26 11:42:35 +0200

    pdf format chooser

---
+++ src/components/AlertBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="alert d-flex align-items-center gap-3" :class="`alert-${color}`" role="alert">
+    <i class="fa-solid" :class="`fa-${icon}`"></i>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps({
+  color: String,
+  icon: String,
+  message: String,
+});
+
+const color = ref(props.color || 'primary');
+const icon = ref(props.icon || 'circle-info');
+const message = ref(props.message);
+</script>
+
+<style lang="scss">
+  .alert > i {
+    font-size: 2rem;
+  }
+</style>
--- src/views/LicenseView.vue
+++ src/views/LicenseView.vue
@@ -2,10 +2,35 @@
   <div class="container my-3">
     <div class="row justify-content-md-center">
       <div class="col-12 col-md-6">
+        <AlertBox
+          message="Selectionnez l'image téléchargée depuis votr espace EDEN puis cliquez sur 'Générer le PDF'"
+        />
         <input class="form-control mb-3" type="file" @change="onChange" :accept="TYPES.join(',')" />
         <img class="img-fluid border rounded mb-3" v-if="image" :src="image" />
-        <button type="button" class="btn btn-dark w-100" @click="onClick" :disabled="!image">
-          Générer le PDF
+        <div class="border rounded mb-3 d-flex p-3 gap-3">
+          <span>Format&nbsp;:</span>
+          <div class="form-check" v-for="[k] of Object.entries(formats)" :key="k">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              :id="`format-${k}`"
+              :value="k"
+              v-model.number="selected"
+            />
+            <label class="form-check-label" :for="`format-${k}`">ID-{{ k }}</label>
+          </div>
+        </div>
+        <button
+          type="button"
+          class="btn btn-dark w-100"
+          @click="onClick"
+          :disabled="!image || processing"
+        >
+          <div v-if="processing" class="spinner-border" role="status">
+            <span class="visually-hidden">Génération...</span>
+          </div>
+          <span v-else>Générer le PDF</span>
         </button>
       </div>
     </div>
@@ -13,6 +38,7 @@
 </template>
 
 <script setup>
+import AlertBox from '@/components/AlertBox.vue';
 import slugify from 'slugify';
 import { ref, computed } from 'vue';
 import { jsPDF } from 'jspdf';
@@ -22,6 +48,14 @@ const image = ref();
 const file = computed(() =>
   slugify(name.value.replaceAll('_', '-').replace(/\.[^.$]+$/, ''), { lower: true })
 );
+const processing = ref(false);
+const selected = ref(1);
+
+const formats = {
+  1: { w: 3.37, h: 2.125 },
+  2: { w: 4.134, h: 2.913 },
+  3: { w: 4.921, h: 3.465 },
+};
 
 const TYPES = ['image/jpeg', 'image/png', 'image/gif'];
 
@@ -35,16 +69,30 @@ function onChange(e) {
   image.value = URL.createObjectURL(file);
 }
 
-function onClick() {
-  const doc = new jsPDF({ format: 'a4', unit: 'in' });
-  doc.setFontSize(12);
-  doc.setLineWidth(0.01);
-  doc.setLineDash([0.04], 0);
-  doc.text(name.value, 1, 1);
-  doc.addImage(image.value, 'png', 1, 1.5, 3.37, 2.125, 'license', 'none', 0);
-  doc.addImage(image.value, 'png', 1 + 3.37, 1.5, 3.37, 2.125, 'license', 'none', 180);
-  doc.roundedRect(1, 1.5, 3.37, 2.125, 0.125, 0.125);
-  doc.roundedRect(1, 1.5 + 2.125, 3.37, 2.125, 0.125, 0.125);
-  doc.save(`${file.value}.pdf`);
+function makePDF() {
+  return new Promise((resolve, reject) => {
+    try {
+      const { w, h } = formats[selected.value];
+      const doc = new jsPDF({ format: 'a4', unit: 'in' });
+      doc.setFontSize(12);
+      doc.setLineWidth(0.01);
+      doc.setLineDash([0.04], 0);
+      doc.text(name.value, 1, 1);
+      doc.addImage(image.value, 'png', 1, 1.5, w, h, 'license', 'none', 0);
+      doc.addImage(image.value, 'png', 1 + w, 1.5, w, h, 'license', 'none', 180);
+      doc.roundedRect(1, 1.5, w, h, 0.125, 0.125);
+      doc.roundedRect(1, 1.5 + h, w, h, 0.125, 0.125);
+      doc.save(`${file.value}.pdf`);
+      resolve(doc);
+    } catch (e) {
+      reject(null);
+    }
+  });
+}
+
+async function onClick() {
+  processing.value = true;
+  await makePDF();
+  processing.value = false;
 }
 </script>
